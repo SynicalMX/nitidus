1e6a4b42e41e6aa34e44960b7d074cf2a119353c
SynicalMX <70176240+SynicalMX@users.noreply.github.com>
2023-01-31 15:04:02 +0000

    add install script

--- install.lua
+++ install.lua
@@ -1,6 +1,23 @@
-local server = "https://raw.githubusercontent.com/SynicalMX/nitidus/"
+local server = "https://raw.githubusercontent.com/SynicalMX/nitidus/master/"
+local dir = "nitidus/"
 
 local function createStructure()
     fs.makeDir("nitidus")
 end
 
+local function f(name)
+    return server..name
+end
+
+local function downloadFile(path)
+    local request = http.get(f(path))
+    local file = fs.open(dir..path, "w")
+    file.write(request.readAll())
+    request.close()
+    file.close()
+end
+
+createStructure()
+downloadFile("src/nitidus.lua")
+downloadFile("src/lib/buttons.lua")
+print("Installed Nitidus v1.0 successfully.")
