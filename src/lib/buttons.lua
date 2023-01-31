99da9077ee31aa0ee62b5409a02b4c5fcdfbe3cd
SynicalMX <70176240+SynicalMX@users.noreply.github.com>
2023-01-31 16:56:29 +0000

    add draw function

--- src/lib/buttons.lua
+++ src/lib/buttons.lua
@@ -28,6 +28,21 @@ function buttons:createButton(xmin, ymax, xmax, ymin, initText)
         buttons:setText(button, text)
     end
 
+    function button:draw()
+        paintutils.drawFilledBox(button.bounds.xmin-1, button.bounds.ymax+1, button.bounds.xmax-1, button.bounds.ymin+1, button.properties.pallete.shadow)
+        paintutils.drawFilledBox(button.bounds.xmin, button.bounds.ymax, button.bounds.xmax, button.bounds.ymin, button.properties.pallete.main)
+
+        -- Click detection
+        parallel.waitForAll(function ()
+            local event, b, x, y = os.pullEvent("mouse_click")
+            if b == 1 then
+                if x >= button.bounds.xmin and x <= button.bounds.xmax and y >= button.bounds.ymin and y <= button.bounds.ymax then
+                    os.queueEvent("button_click", button, x, y)
+                end
+            end
+        end)
+    end
+
     return button
 end
 
