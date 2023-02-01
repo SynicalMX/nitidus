3705e15e650b59320245f0c69ce688177c94bab8
SynicalMX <70176240+SynicalMX@users.noreply.github.com>
2023-02-01 18:06:50 +0000

    add more checks for preinstall

--- install.lua
+++ install.lua
@@ -1,10 +1,11 @@
 local server = "https://raw.githubusercontent.com/SynicalMX/nitidus/master/"
 local dir = "nitidus/"
+local install = true
 
 local function createStructure()
-    if fs.exists('/nitidus') ~= true then
+    if fs.exists('/nitidus') == true then
         print("Nitidus is already installed.")
-        os.exit(0)
+        install = false
     end
     fs.makeDir("nitidus")
     fs.makeDir("nitidus/lib")
@@ -15,6 +16,9 @@ local function f(name)
 end
 
 local function downloadFile(path, out)
+    if not install then
+        return
+    end
     local request = http.get(f(path))
     local file = fs.open(out, "w")
     file.write(request.readAll())
@@ -25,4 +29,6 @@ end
 createStructure()
 downloadFile("src/nitidus.lua", "nitidus/init.lua")
 downloadFile("src/lib/buttons.lua", "nitidus/lib/buttons.lua")
-print("Installed Nitidus v1.0 successfully.")
+if install then
+    print("Installed Nitidus v1.0 successfully.")
+end
--- src/lib/buttons.lua
+++ src/lib/buttons.lua
@@ -1,44 +1,57 @@
 local buttons = {}
 
-function buttons:createButton(xmin, ymax, xmax, ymin, initText)
-    local button = {}
-
+function buttons:createButton(xmin, ymax, xmax, ymin, onclick, initText)
     if not initText then
         initText = "Button"
     end
 
-    local bounds = {
-        ["xmin"] = xmin,
-        ["ymax"] = ymax,
-        ["xmax"] = xmax,
-        ["ymin"] = ymin
-    }
+    if not onclick then
+        error("Button '"..initText.."' was not provided an onclick function")
+    end
 
-    local properties = {
-        ["pallete"] = {
-            ["main"] = colors.lime,
-            ["shadow"] = colors.lightGray
+    local button = {
+        ["properties"] = {
+            ["pallete"] = {
+                ["main"] = colors.lime,
+                ["shadow"] = colors.lightGray,
+                ["text"] = colors.white,
+            },
+            ["text"] = initText
         },
-        ["text"] = initText
+        ["bounds"] = {
+            ["xmin"] = xmin,
+            ["ymax"] = ymax,
+            ["xmax"] = xmax,
+            ["ymin"] = ymin
+        }
     }
-
-    table.insert(button, bounds)
-    table.insert(button, properties)
 
     function button:setText(text)
-        buttons:setText(button, text)
+        buttons:setText(self, text)
     end
 
     function button:draw()
-        paintutils.drawFilledBox(button.bounds.xmin-1, button.bounds.ymax+1, button.bounds.xmax-1, button.bounds.ymin+1, button.properties.pallete.shadow)
-        paintutils.drawFilledBox(button.bounds.xmin, button.bounds.ymax, button.bounds.xmax, button.bounds.ymin, button.properties.pallete.main)
+        -- Get center to write button text
+        local cenX = math.floor(((self.bounds.xmin + self.bounds.xmax) / 2) - (string.len(self.properties.text) / 2))
+        local cenY = math.floor((self.bounds.ymin + self.bounds.ymax) / 2)
+
+        -- Draw shadow
+        paintutils.drawFilledBox(self.bounds.xmin-1, self.bounds.ymax+1, self.bounds.xmax-1, self.bounds.ymin+1, self.properties.pallete.shadow)
+        -- Draw button
+        paintutils.drawFilledBox(self.bounds.xmin, self.bounds.ymax, self.bounds.xmax, self.bounds.ymin, self.properties.pallete.main)
+
+        -- Write button text
+        term.setCursorPos(cenX, cenY)
+        term.setBackgroundColor(self.properties.pallete.main)
+        term.setTextColor(self.properties.pallete.text)
+        term.write(self.properties.text)
 
         -- Click detection
         parallel.waitForAll(function ()
-            local event, b, x, y = os.pullEvent("mouse_click")
-            if b == 1 then
-                if x >= button.bounds.xmin and x <= button.bounds.xmax and y >= button.bounds.ymin and y <= button.bounds.ymax then
-                    os.queueEvent("button_click", button, x, y)
+            while true do
+                local event, b, x, y = os.pullEvent("mouse_click")
+                if b == 1 and x >= self.bounds.xmin and x <= self.bounds.xmax and y >= self.bounds.ymin and y <= self.bounds.ymax then
+                    self.onclick()
                 end
             end
         end)
